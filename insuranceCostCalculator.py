eb00f11e239c34ffe692032bff05c8b4410ffa6a
Priyanka Tiwari <48170152+priyankatiwari05@users.noreply.github.com>
2022-10-12 15:53:01 +0530

    insuranceCostCalculatorFunction (#4)
    * update/rbiindexfile
    
    * unittest_added
    
    * unittest_added
    
    * Create requirements.txt
    
    * Update requirements.txt
    
    * Update unittest-workflow.yml
    
    * unittest_updated
    
    * removing_yamls
    
    * Rename InsuranceCostCalculator.py to insuranceCostCalculator.py
    
    * update/unittest_yaml_and_test
    
    * update/unittest_yaml
    
    * Update insuranceCostCalculator.py
    
    * Update unittest-workflow.yml
    
    * Update requirements.txt
    
    * Update insuranceCostCalculator.py
    
    * Update insuranceCostCalculator.py
    
    * Update unittest-workflow.yml
    
    Co-authored-by: Priyanka Tiwari <priyankatiwari2908@gmail.com>

--- insuranceCostCalculator.py
+++ insuranceCostCalculator.py
@@ -10,12 +10,11 @@ def calculate_insurance_cost (comm_id, amount, date) :
     if result_amt != 0 :
         with open('rbiIndex.json') as json_file :
             dictionary = json.load(json_file)
-            print(":::: ", dictionary.get(comm_id).get('INDEXES').get('INDX042012')) 
-            print(":::: ", dictionary.get(comm_id).get('INDEXES').get('INDX'+str(parser.parse(date).strftime("%m")) + str(parser.parse(date).year)))
-           #yeartoSearch =  'INDX'+str(parser.parse(date).strftime("%m") +'2012')
+            print(":::: ", dictionary[comm_id]['INDEXES']['INDX042012'])
+            print(":::: ", dictionary[comm_id]['INDEXES']['INDX'+str(parser.parse(date).strftime("%m")) + str(parser.parse(date).year)])
             yeartoSearch = 'INDX042012'
             yearFromFar = 'INDX'+str(parser.parse(date).strftime("%m")) + str(parser.parse(date).year)
-            indexIncreased = dictionary.get(comm_id).get('INDEXES').get(yearFromFar)/dictionary.get(comm_id).get('INDEXES').get(yeartoSearch)
+            indexIncreased = dictionary[comm_id]['INDEXES'][yearFromFar]/dictionary[comm_id]['INDEXES'][yeartoSearch]
             print ("indexIncreased :: ", indexIncreased)
         json_file.close()
     return round((result_amt * indexIncreased),2)
@@ -27,36 +26,37 @@ def lambda_handler(event, context):
         body = json.loads(event['body'])
         financial_doc_json = body['financial_doc_json']
         policy_doc_json = body['policy_doc_json']
-   #with open('FarJson.json') as far_json :
-        financial_doc_dict = json.load(financial_doc_json)
-        for key in financial_doc_dict.keys() :
-            if (key == "Date") :
-                date = financial_doc_dict.get(key)
-                print("Date :: ",date)
+        for key in financial_doc_json.keys() :
+            if (key == "Date") :               
+                date = financial_doc_json[key]
             
             if(key == "Buildings") :
-                for building in financial_doc_dict.get(key) :
-                    cost = calculate_insurance_cost(building.get('1316040002'),building.get('amount'), date)
+                for building in financial_doc_json[key] :
+                    cost = calculate_insurance_cost('1316040002',building['amount'], date)
                     del building["amount"]
                     building.update(suggested_insurance_cost = cost)
+                    financial_doc_json[key]=building
 
             if(key == "Plant and Machinery") :
-                for plantAndMachin in financial_doc_dict.get(key) :
-                    cost = calculate_insurance_cost('1316040002', plantAndMachin.get('amount'), date)
+                for plantAndMachin in financial_doc_json[key] :
+                    cost = calculate_insurance_cost('1316040002', plantAndMachin['amount'], date)
                     del plantAndMachin["amount"]
                     plantAndMachin.update(suggested_insurance_cost = cost)
+                    financial_doc_json[key]=plantAndMachin
 
             if(key == "Office Equipments") :
-                for officeEquipments in financial_doc_dict.get(key) :
-                    cost = calculate_insurance_cost('1316040002', officeEquipments.get('amount'), date)
+                for officeEquipments in financial_doc_json[key] :
+                    cost = calculate_insurance_cost('1316040002', officeEquipments['amount'], date)
                     del officeEquipments["amount"]
                     officeEquipments.update(suggested_insurance_cost = cost)
+                    financial_doc_json[key]=officeEquipments
 
             if(key == "Furniture & Fixtures") :
-                for furnitureAndFixture in financial_doc_dict.get(key) :
-                    cost = calculate_insurance_cost('1316040002', furnitureAndFixture.get('amount'), date)
+                for furnitureAndFixture in financial_doc_json[key] :
+                    cost = calculate_insurance_cost('1316040002', furnitureAndFixture['amount'], date)
                     del furnitureAndFixture["amount"]
                     furnitureAndFixture.update(suggested_insurance_cost = cost)
+                    financial_doc_json[key]=furnitureAndFixture
 
     except Exception as exp:
         print("##### Exception occured while processing :: ", exp)
@@ -66,8 +66,8 @@ def lambda_handler(event, context):
 
         }
     print ("Success")
-    print("Dict ::: ", financial_doc_dict)
+    print("Dict ::: ", financial_doc_json)
     return {
         'statusCode': 200,
-        'body': json.dumps(financial_doc_dict)
+        'body': json.dumps(financial_doc_json)
     }
